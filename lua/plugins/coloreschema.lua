e0f976025a28e9b1f39dbe32edec9d4f9a5524d9
Mohamed-Ridha <medbloul2002@gmail.com>
2024-09-07 13:45:00 +0100

    add new plugins -> colorshema and icons

---
+++ lua/plugins/coloreschema.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  --TODO: line for activate colorscheme ->  colorscheme dogrun
+  {
+    "wadackel/vim-dogrun",
+  },
+}
